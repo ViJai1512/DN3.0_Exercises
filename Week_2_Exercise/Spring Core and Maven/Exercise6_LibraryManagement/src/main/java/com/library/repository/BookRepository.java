52f098bcbd6b430271e3f944de959a5b4fd8c29a
Vijai Karthik <113586713+ViJai1512@users.noreply.github.com>
2024-08-06 13:18:25 +0530

    Spring_Maven

---
+++ Week_2_Exercise/Spring Core and Maven/Exercise6_LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+    public void findAll() {
+        System.out.println("Finding all books in the repository.");
+    }
+}
